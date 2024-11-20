36ae568a1b6e542b93844098e452881c0364a187
MohamedHarby18 <155351502+MohamedHarby18@users.noreply.github.com>
2024-11-20 16:52:37 +0200

    Add files via upload
    project Start!

---
+++ inventory_Managemnt_System/src/main/java/com/mycompany/inventory_managemnt_system/Inventory_Managemnt_System.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventory_managemnt_system;
+
+public class Inventory_Managemnt_System {
+
+    public static void main(String[] args) {
+        System.out.println("Fuck Kareem");
+    }
+}
