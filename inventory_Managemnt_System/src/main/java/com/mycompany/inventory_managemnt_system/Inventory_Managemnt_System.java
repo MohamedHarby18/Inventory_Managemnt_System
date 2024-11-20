4cfb521528f39715220548c659aac22053d3d9f5
MohamedHarby18 <155351502+MohamedHarby18@users.noreply.github.com>
2024-11-20 17:29:11 +0200

    ..

--- inventory_Managemnt_System/src/main/java/com/mycompany/inventory_managemnt_system/Inventory_Managemnt_System.java
+++ inventory_Managemnt_System/src/main/java/com/mycompany/inventory_managemnt_system/Inventory_Managemnt_System.java
@@ -7,6 +7,7 @@ package com.mycompany.inventory_managemnt_system;
 public class Inventory_Managemnt_System {
 
     public static void main(String[] args) {
+        System.out.println("Hello Every one");
         System.out.println("Fuck Kareem");
     }
 }
